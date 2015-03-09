586a2c75aebcd4655fa1ed22760dec8387480189
rodrigoconcham <rodrigoconcham@gmail.com>
2015-03-09 18:02:58 -0300

    avance con agregar imagenes

--- autolote/Scripts/Autolote-Imagenes.js
+++ autolote/Scripts/Autolote-Imagenes.js
@@ -37,3 +37,15 @@ $("#add").live("click", function () {
 
     return false;
 });
+
+function GetRandomGUI() {
+
+    var val ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' .replace(/[xy]/g, function(c){
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+            
+    });
+
+
+    return val;
+}
