f105b634456ebacd374091614acc90125a720f90
rodrigoconcham <rodrigoconcham@gmail.com>
2015-03-18 17:51:30 -0400

    avance js

--- autolote/Scripts/Autolote-MarcasModelos.js
+++ autolote/Scripts/Autolote-MarcasModelos.js
@@ -1,1 +1,27 @@
-﻿
+﻿$(function(){
+
+    $('#MarcasId').change(function() {
+
+        if ($('#modelosId').val() ) {
+            $.getJSON('/Modelos/ListaModelosPorMarca/' + $('#MarcasId').val(),function (data) {
+
+                var items =$('#ModelosId').children()[0].outerHTML;
+
+
+                $.each(data, function(i,modelo) {
+
+                    items += "<option value ='" + modelo.Id + "'>" + modelo.Modelo + "</option>"
+
+
+                    });
+        
+                $('#ModelosId').html(items);
+                  
+            
+            
+                });
+        }
+
+
+
+});
