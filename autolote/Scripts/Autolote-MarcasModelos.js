9507e80a051f725fb98d05efdb950c086aab8847
rodrigoconcham <rodrigoconcham@gmail.com>
2015-03-19 17:50:34 -0400

    avance marcas modelo

--- autolote/Scripts/Autolote-MarcasModelos.js
+++ autolote/Scripts/Autolote-MarcasModelos.js
@@ -1,26 +1,102 @@
-﻿$(function(){
+﻿$(function () {
 
-    $('#MarcasId').change(function() {
+    $('#MarcasId').change(function () {
 
-        if ($('#modelosId').val() ) {
-            $.getJSON('/Modelos/ListaModelosPorMarca/' + $('#MarcasId').val(),function (data) {
+        if ($('#modelosId').val()) {
+            $.getJSON('/Modelos/ListaModelosPorMarca/' + $('#MarcasId').val(), function (data) {
 
-                var items =$('#ModelosId').children()[0].outerHTML;
+                var items = $('#ModelosId').children()[0].outerHTML;
 
 
-                $.each(data, function(i,modelo) {
+                $.each(data, function (i, modelo) {
 
                     items += "<option value ='" + modelo.Id + "'>" + modelo.Modelo + "</option>"
 
 
-                    });
-        
-                $('#ModelosId').html(items);
-                  
-            
-            
                 });
+
+                $('#ModelosId').html(items);
+
+
+
+            });
         }
+
+
+
+    });
+    //Obtener el Id de la marca seleccionada
+    var id = 0;
+
+
+    if ($('#Id').val()) {
+        id = $('#Id').val();
+
+    }
+
+
+    $.getJSON('/Marcas/ListaMarcas/' + id, function (data) {
+
+        var items = $('#MarcasId')[0].innerHTML;
+        $.each(data, function (i, marca) {
+
+            if (marca.selected) {
+                items += "<option value='" + marca.id + "' selected>" + marca.Marca + "</option>";
+                MostrarModelosPorMarca();
+            }
+
+            else {
+
+                items += "<option value='" + marca.id + "'>" + marca.Marca + "</option>";
+
+
+            }
+
+
+        });
+
+        $('#MarcasId').html(items);
+
+
+
+    });
+
+
+
+    function MostrarModelosPorMarca() {
+
+        $.getJSON('/Marcas/ListaModelosPorAutomovil/' + $('#Id').val(), function (data) {
+
+            var items = $('#ModelosId')[0].innerHTML;
+            $.each(data, function (i, marca) {
+
+                if (marca.selected) {
+                    items += "<option value='" + marca.id + "' selected>" + marca.Marca + "</option>";
+                    MostrarModelosPorMarca();
+                }
+
+                else {
+
+                    items += "<option value='" + marca.id + "'>" + marca.Marca + "</option>";
+
+
+                }
+
+
+            });
+
+            $('#ModelosId').html(items);
+
+
+
+
+        });
+   
+
+        }
+ 
+
+
 
 
 
